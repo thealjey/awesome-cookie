79a8e25bea1057aa5beeee0252dbc2f546c7f9ad
Eugene Kuzmenko <aljey1972@gmail.com>
2014-04-16 14:16:24 +0000

    initial commit

--- README.md
+++ README.md
@@ -2,3 +2,19 @@ awesome-cookie
 ==============
 
 A tiny script that takes care of all the cookie related problems in JavaScript
+
+```javascript
+/** Usage example */
+//set a cookie
+Cookie.set('name', 'John Doe');
+//optionally specify the number of days for which the cookie should be valid
+Cookie.set('age', 35, 365);
+//retrieve a value
+Cookie.get('name');
+//check if a value exists
+if (null !== Cookie.get('bogus')) {}
+//or
+if (Cookie.get('bogus')) {}
+//remove a cookie
+Cookie.remove('name');
+```
---
+++ awesome-cookie.js
@@ -0,0 +1,18 @@
+/**
+ * @author Eugene Kuzmenko
+ * @description A tiny script that takes care of all the cookie related problems in JavaScript
+ * @license MIT
+ */
+var Cookie = (function () {
+  var r = /=|;/, p = ';path=/' + (/\d+\.\d+\.\d+\.\d+/.test(document.domain) ? '' : ';domain=.' + location.host);
+  function set(n, v, d) {
+    var e = new Date;
+    d && e.setDate(e.getDate() + d);
+    document.cookie = [n, '=', v, ';expires=', e, p].join('');
+  }
+  return {
+    get: function (n) { return (n = (document.cookie + ';').match(new RegExp(n + '=.*;'))) && n[0].split(r)[1] },
+    set: set,
+    remove: function (n) { set(n, '', -1) }
+  };
+}());
