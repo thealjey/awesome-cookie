0bf162991f7a739ca23ed9e736d7bda9a3edcdba
Eugene Kuzmenko <aljey1972@gmail.com>
2014-08-28 16:11:35 +0300

    more effective use of the document object

--- awesome-cookie.js
+++ awesome-cookie.js
@@ -3,16 +3,16 @@
  * @description A tiny script that takes care of all the cookie related problems in JavaScript
  * @license MIT
  */
-var Cookie = (function () {
-  var r = /=|;/, p = ';path=/' + (/\d+\.\d+\.\d+\.\d+/.test(document.domain) ? '' : ';domain=.' + location.host);
-  function set(n, v, d) {
+var Cookie = (function (d) {
+  var r = /=|;/, p = ';path=/' + (/\d+\.\d+\.\d+\.\d+/.test(d.domain) ? '' : ';domain=.' + location.host);
+  function set(n, v, t) {
     var e = new Date;
-    d && e.setDate(e.getDate() + d);
-    document.cookie = [n, '=', v, ';expires=', e.toUTCString(), p].join('');
+    t && e.setDate(e.getDate() + t);
+    d.cookie = [n, '=', v, ';expires=', e.toUTCString(), p].join('');
   }
   return {
-    get: function (n) { return (n = (document.cookie + ';').match(new RegExp(n + '=.*;'))) && n[0].split(r)[1] },
+    get: function (n) { return (n = (d.cookie + ';').match(new RegExp(n + '=.*;'))) && n[0].split(r)[1] },
     set: set,
     remove: function (n) { set(n, '', -1) }
   };
-}());
+}(document));
